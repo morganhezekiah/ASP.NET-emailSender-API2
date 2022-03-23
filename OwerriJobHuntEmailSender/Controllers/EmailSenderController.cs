70884960c9bc0f9ee3b9cc3f96d3d15e6d00b9f4
cyba <morganhezekiah59@gmail.com>
2022-03-23 21:08:38 +0100

    first commit

---
+++ OwerriJobHuntEmailSender/Controllers/EmailSenderController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using OwerriJobHuntEmailSender.Models;
+using System.Net;
+
+namespace OwerriJobHuntEmailSender.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailSenderController :ControllerBase
+    {
+        [HttpPost]
+        [Route("send")]
+        public string SendMail([FromBody] EmailSenderModel emailSender)
+        {
+            MailAddress senderAddress = new MailAddress("morganhezekiah11@gmail.com", "OwerriJobHunt");
+            MailMessage mailMessage = new MailMessage()
+            {
+                From = senderAddress,
+                IsBodyHtml = true,
+                Subject = emailSender.Subject,
+                Body = emailSender.TemplateBody
+
+            };
+            mailMessage.To.Add(emailSender.SendingTo);
+
+            SmtpClient smtpClient = new SmtpClient()
+            {
+                UseDefaultCredentials = false,
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Credentials = new NetworkCredential("morganhezekiah11@gmail.com", "wmiixksozsqletoc"),
+                EnableSsl = true
+            };
+
+            try
+            {
+                smtpClient.Send(mailMessage);
+            }
+            catch (Exception e)
+            {
+                return String.Format("Email Error: {0}", e.Message.ToString());
+            }
+
+            return "Message Sent";
+        }
+    }
+}
---
+++ OwerriJobHuntEmailSender/Models/EmailSenderModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OwerriJobHuntEmailSender.Models
+{
+    public class EmailSenderModel
+    {
+        [Required]
+        public string SendingTo { get; set; }
+
+        [Required]
+        public string TemplateBody { get; set; }
+
+
+        [Required]
+        public string Subject { get; set; }
+    }
+}
